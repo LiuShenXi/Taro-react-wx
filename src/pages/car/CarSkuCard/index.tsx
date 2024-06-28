cb5bccbddd10d383d88af79a09b06b4a822d8003
lc309890 <lc309890.jx@chinatelecom.cn>
2024-06-28 18:12:57 +0800

    完善购物车逻辑

--- src/pages/car/CarSkuCard/index.tsx
+++ src/pages/car/CarSkuCard/index.tsx
@@ -1,21 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Card } from "@antmjs/vantui";
 import { View } from "@tarojs/components";
 import { Checkbox, Divider, Stepper } from "@antmjs/vantui";
 
 import "./index.less";
 
-// 接受 props
-const CarSkuCard: React.FC<{ skuInfo: { num: number, title: string, desc: string, price: string } }> = ({ skuInfo }) => {
-  const [value, setValue] = useState(false);
+const CarSkuCard: React.FC<{ skuInfo: { num: number, title: string, desc: string, price: string }, isSelected: boolean, onSelect: (title: string, selected: boolean) => void, onCountChange: (title: string, num: number) => void }> = ({ skuInfo, isSelected, onSelect, onCountChange }) => {
+  const [value, setValue] = useState(isSelected);
+  const [num, setNum] = useState(skuInfo.num);
 
-  const renderNum = () => {
-    return (
-      <View className="num-block">
-        <Stepper value={skuInfo.num}></Stepper>
-      </View>
-    );
+  useEffect(() => {
+    setValue(isSelected);
+  }, [isSelected]);
+
+  useEffect(() => {
+    setNum(skuInfo.num);
+  }, [skuInfo.num]);
+
+  const handleChange = (e) => {
+    const selected = e.detail;
+    setValue(selected);
+    onSelect(skuInfo.title, selected);
   };
+
+  const handleCountChange = (e) => {
+    const newNum = e.detail;
+    setNum(newNum);
+    onCountChange(skuInfo.title, newNum);
+  };
+
+  const renderNum = () => (
+    <View className="num-block">
+      <Stepper value={num} onChange={handleCountChange} />
+    </View>
+  );
 
   return (
     <View>
@@ -25,8 +43,8 @@ const CarSkuCard: React.FC<{ skuInfo: { num: number, title: string, desc: string
             className="check"
             checkedColor="red"
             value={value}
-            onChange={(e) => setValue(e.detail)}
-          ></Checkbox>
+            onChange={handleChange}
+          />
         </View>
         <Card
           className="card"
--- src/pages/car/index.tsx
+++ src/pages/car/index.tsx
@@ -1,20 +1,70 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { View } from '@tarojs/components';
-import { Divider, SubmitBar } from "@antmjs/vantui";
+import React, { useState, useEffect } from 'react';
+import { View, Text } from '@tarojs/components';
+import { Cell, Checkbox, SubmitBar } from "@antmjs/vantui";
 import CarSkuCard from './CarSkuCard';
-import { beautyServices } from './service'
+import { beautyServices } from './service';
+
+import './index.less';
+
+interface BeautyService {
+  num: number;
+  title: string;
+  desc: string;
+  price: string;
+  isSelected?: boolean;
+}
 
 const Car: React.FC = () => {
+  const [selectAll, setSelectAll] = useState(false);
+  const [items, setItems] = useState<BeautyService[]>(beautyServices.map(item => ({ ...item, isSelected: false })));
+  const [totalPrice, setTotalPrice] = useState(0);
 
-  return <View>
-    <Divider></Divider>
-    {beautyServices.map(item => <CarSkuCard key={item.title} skuInfo={item} />)}
-    <SubmitBar
-      price={3050}
-      buttonText="提交订单"
-      onSubmit={() => console.info('提交')}
-    />
-  </View>
+  useEffect(() => {
+    // 更新合计金额
+    const total = items.reduce((sum, item) => item.isSelected ? sum + item.num * parseFloat(item.price) : sum, 0);
+    setTotalPrice(total);
+  }, [items]);
+
+  const setAll = (e) => {
+    const selected: boolean = e.detail;
+    setSelectAll(selected);
+    setItems(items.map(item => ({ ...item, isSelected: selected })));
+  };
+
+  const handleItemSelect = (title, selected) => {
+    setItems(prev => prev.map(item => item.title === title ? { ...item, isSelected: selected } : item));
+  };
+
+  const handleItemCountChange = (title, num) => {
+    setItems(prev => prev.map(item => item.title === title ? { ...item, num: parseInt(num, 10) } : item));
+  };
+
+  const renderTitle = () => (
+    <View className='title-wp'>
+      <Checkbox value={selectAll} onChange={setAll} checkedColor="red" />
+      <Text>全选</Text>
+    </View>
+  );
+
+  return (
+    <View>
+      <Cell renderTitle={renderTitle()} />
+      {items.map(item => (
+        <CarSkuCard
+          key={item.title}
+          skuInfo={item}
+          isSelected={item.isSelected ? true : false}
+          onSelect={handleItemSelect}
+          onCountChange={handleItemCountChange}
+        />
+      ))}
+      <SubmitBar
+        price={totalPrice * 100} // price in cents
+        buttonText="提交订单"
+        onSubmit={() => console.info('提交')}
+      />
+    </View>
+  );
 };
 
 export default Car;
